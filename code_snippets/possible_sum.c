d4e0fa3b6a6cdfc159450a00b21ca485b0d196f2
PrajnaYaji18 <prajna@amagi.com>
2020-05-01 10:09:54 +0530

    first add

--- code_snippets/possible_sum.c
+++ code_snippets/possible_sum.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include "possible_sum.c"
@@ -28,3 +29,35 @@ int main()
 
 
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include "possible_sum.c"
+
+int main()
+{
+    int data[]={3,5,10};
+    int value=15;
+     value=value+1;
+    int arr[value];
+    memset(arr,0,value*sizeof(arr[0]));
+    int i,j;
+    int x,k;
+    arr[0]=1;
+    for (i=0;i<3;i++)
+    {
+
+              k=0;
+          for (j=data[i];j<value;j++)
+          {
+              arr[j]=arr[j]+arr[k++];
+          }
+      for (x=0;x<value;x++)
+        printf("%d ",arr[x]);
+        printf("\n");
+    }
+
+
+
+}
+>>>>>>> first add
--- code_snippets/substring_bit_manipulation.c
+++ code_snippets/substring_bit_manipulation.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,3 +21,27 @@ for(i=0;i<size;i++)
     printf("\n");
 }
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "subset.c"
+int main()
+{
+   char data[]={'a','b','c'};
+   int set_size,i;
+   set_size=3;
+
+   int j;
+int size=pow(2,set_size);
+for(i=0;i<size;i++)
+{
+    for(j=0;j<set_size;j++)
+    {
+        if (i&(1<<j))
+            printf("%c",data[j]);
+    }
+    printf("\n");
+}
+}
+>>>>>>> first add
